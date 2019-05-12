bcb5c519872df82d155800085bc4cc9a7bbe4a41
marianamendivil <mariana.mendidu@gmail.com>
2019-05-12 03:04:14 -0500

    initial app

---
+++ www/js/main.js
@@ -0,0 +1,36 @@
+window.onload = init;
+function init() {
+
+    //Navigation
+    document.getElementById("loadp").style.display = "block";
+
+    var time = document.getElementById("time");
+    setTimeout(()=>{document.getElementById("loadp").style.display = "none"; 
+        document.getElementById("initialp").style.display = "block";}, 2000);
+
+    var registroButton = document.getElementById("registroButton");
+    registroButton.addEventListener('click', navigate('initialp', 'registrop'));
+
+    var registrarse = document.getElementById("registrarse");
+    registrarse.addEventListener('click', function(){
+        registrar();
+    });
+
+    var registroBackButton = document.getElementById("registroBackButton");
+    registroBackButton.addEventListener('click', navigate('registrop', 'initialp'));
+}
+
+var navigate = function(actual, next) {
+    return function() {
+        hide(actual);
+        show(next);
+    }
+}
+
+function show (id) {
+        document.getElementById(id).style.display = "block";
+    }
+
+function hide (id) {
+    document.getElementById(id).style.display = "none";
+}
