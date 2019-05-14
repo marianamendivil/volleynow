7b8955c25dc6f4fb9f652bb47fe6b6065d516c06
Santiago Restrepo <santiago_relo@hotmail.com>
2019-05-13 19:04:50 -0500

    v0.1 validacion registro

--- www/js/registro.js
+++ www/js/registro.js
@@ -6,21 +6,43 @@ function registrar(){
     var posicionF= document.querySelector('input[name="PosicionF"]:checked').value;
     var usuario= document.getElementById("Usuario").value;
     var contraseña= document.getElementById("Contraseña").value;
-    var verificar = validarnull(nombre,apellido,correo,posicionF,usuario,contraseña);
+    var verificar = validarnull(nombre,apellido,correo,usuario,contraseña);
 
     if(verificar == true){
-     var setRegister = firebase.database().ref('Usuarios');
-     setRegister.child(usuario).set({
-        Apellido : apellido,
-        Contraseña : contraseña,
-        Correo : correo,
-        Nombre : nombre,
-        Posicion : posicionF
-     });
+
+        var ref = firebase.database().ref('Usuarios/'+usuario);
+        var Boolver =  true;
+        ref.once('value')
+           .then(function(snapshot) {
+            var ver = snapshot.child("Contraseña").val();
+            
+            if(ver != null){
+                Boolver = false;
+            }
+        });
+
+
+        console.log(Boolean);
+        if(Boolver == true){
+
+            var setRegister = firebase.database().ref('Usuarios');
+            setRegister.child(usuario).set({
+                Apellido : apellido,
+                Contraseña : contraseña,
+                Correo : correo,
+                Nombre : nombre,
+                Posicion : posicionF
+            });
+        }
+        else{
+            window.alert("Nombre de usuario en uso");
+            document.getElementById("Usuario").classList.add("alert-danger");
+
+        }
     }
 }
 
-function validarnull(nombre,apellido,correo,posicionF,usuario,contraseña){
+function validarnull(nombre,apellido,correo,usuario,contraseña){
     var verificar=true;
     console.log("aqui");
     if(nombre==""){
