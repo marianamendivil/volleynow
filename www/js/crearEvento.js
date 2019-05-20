b18177c05bf7bbea795d368da1b7d5b3702b7d3d
Santiago Restrepo <santiago_relo@hotmail.com>
2019-05-19 22:40:23 -0500

    v0.1 de Crear evento

---
+++ www/js/crearEvento.js
@@ -0,0 +1,10 @@
+function crearevento(){
+    var usuario= document.getElementById("user").value;
+    var Hora = document.getElementById("Hora").value;
+    var Calendario= document.getElementById("Calendario").value;
+    var Lugar= document.getElementById("Lugar").value;
+    var Descripcion= document.getElementById("Descripcion").value;
+    var Puntualidad= document.getElementById("Puntualidad").value;
+    
+    
+}
