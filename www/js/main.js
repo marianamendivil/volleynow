16355bcfacc62ff5f17b3069c00491d75cff2e40
marianamendivil <mariana.mendidu@gmail.com>
2019-05-20 01:10:04 -0500

    eventos

--- www/js/main.js
+++ www/js/main.js
@@ -16,6 +16,11 @@ function init() {
         registrar();
     });
 
+    var entrar = document.getElementById("entrar");
+    entrar.addEventListener('click', function(){
+        validar();
+    });
+
     var registroBackButton = document.getElementById("registroBackButton");
     registroBackButton.addEventListener('click', navigate('registrop', 'initialp'));
 }
@@ -33,4 +38,17 @@ function show (id) {
 
 function hide (id) {
     document.getElementById(id).style.display = "none";
+}
+
+//SIDEBAR
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
---
+++ www/js/validarPass.js
@@ -0,0 +1,34 @@
+function validar(){
+    var  Usuario = document.getElementById("user").value;
+    var Contraseña = document.getElementById("pwd").value;
+    
+    var ref = firebase.database().ref('Usuarios/'+Usuario);
+   if(Usuario!=""){
+       if(Contraseña!=""){
+            ref.once('value')
+            .then(function(snapshot) {
+                try{
+                var contraseña = snapshot.val().Contraseña;
+                var result = JSON.stringify(Contraseña).localeCompare(JSON.stringify(contraseña));
+
+                if(result == 0){
+                    //window.alert("usuario y contraseña correcta");
+                    console.log("first");
+                    navigate('initialp','eventosp');
+                    console.log("ajá");
+                }
+                else{
+                    window.alert("usuario y contraseña incorrecta");
+                }
+                }catch{
+                    window.alert("usuario y contraseña incorrecta");
+                
+                }
+                });
+        }else{
+            document.getElementById("pwd").classList.add("alert-danger");
+        }
+    }else{
+        document.getElementById("user").classList.add("alert-danger");
+    }
+}
